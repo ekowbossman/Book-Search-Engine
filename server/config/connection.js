52924c904e98e7a838b739b09efeee317df05920
ekowbossman <ekowbossman@gmail.com>
2022-08-20 01:49:33 -0400

    added database url to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ekowbossman:bossman1982@cluster0.uewnktn.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ekowbossman:bossman1982@cluster0.uewnktn.mongodb.net/book-search-engine?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,

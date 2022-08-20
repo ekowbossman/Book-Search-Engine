61708a29e369fc086e09610fa06ad058f9cd4515
ekowbossman <ekowbossman@gmail.com>
2022-08-20 00:34:48 -0400

    reterive files from githuh

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ekowbossman:bossman1982@cluster0.uewnktn.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: true,
+  
 });
 
 module.exports = mongoose.connection;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const secret = 'mysecretsshhhhh';
-const expiration = '12h';
+const expiration = '2h';
 
 module.exports = {
   authMiddleware: function({ req }) {

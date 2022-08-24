8a20f1a55d08d19c61cd65d1d55f17b4f7a7b04d
ekowbossman <ekowbossman@gmail.com>
2022-08-23 20:41:40 -0400

    update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ekowbossman:bossman1982@cluster0.uewnktn.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
+  // useCreateIndex: true,
+  // useFindAndModify: true,
   
 });
 
